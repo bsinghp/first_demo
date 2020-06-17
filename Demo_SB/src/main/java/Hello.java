915b5f3702afeb58e24b663769400f88462471c7
Bhagwan Singh <67056471+bhagsingh@users.noreply.github.com>
2020-06-17 18:37:53 +0530

    sonar + jenkins integration

---
+++ Demo_SB/src/main/java/Hello.java
@@ -0,0 +1,10 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+		//demo
+		System.out.println("Demo of Sonar");
+
+	}
+
+}
---
+++ Demo_SB/src/main/resources/com/KeyOpp.java
@@ -0,0 +1,10 @@
+package com;
+
+public class KeyOpp {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+
+	}
+
+}
